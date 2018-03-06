1b9933fd44b579d71da8dbf7a2329d8dbff20e3e
jina <hjina4004@gmail.com>
2018-03-06 18:12:47 +0900

    공지 사항 (firebase 연동)

--- src/pages/notice/notice.module.ts
+++ src/pages/notice/notice.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { NoticePage } from './notice';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { NoticePage } from './notice';
   ],
   imports: [
     IonicPageModule.forChild(NoticePage),
+    TranslateModule.forChild()
   ],
 })
 export class NoticePageModule {}
--- src/pages/notice/notice.ts
+++ src/pages/notice/notice.ts
@@ -1,5 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, Navbar } from 'ionic-angular';
+import { IonicPage, Navbar } from 'ionic-angular';
+
+import { TranslateService } from '@ngx-translate/core';
+import { GlobalFunction } from '../../providers/global-function';
+
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { INotice } from '../../models/notice.m';
 
 /**
  * Generated class for the NoticePage page.
@@ -17,18 +26,58 @@ export class NoticePage {
 
   @ViewChild(Navbar) navbar: Navbar;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {    
-  }
+  selectedItem = null;
+
+  notices: Observable<INotice[]>;
+
+  constructor(
+    private afDB: AngularFireDatabase,
+    private translate: TranslateService,
+    private globalFunction: GlobalFunction
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NoticePage');
-
     this.navbar.backButtonClick = (e:UIEvent) => {
-      let navOptions = {
-        animation: 'ios-transition'
-      };
-      this.navCtrl.pop(navOptions);
+      this.globalFunction.moveBack();
+    }
+
+    this.loadData();
+  }
+
+  ionViewDidLeave() {
+  }
+
+  swipeEvent(ev) {
+    if (ev.direction == 4) {
+      this.globalFunction.moveBack();
     }
   }
 
+  loadData() {
+    const itemsRef = this.afDB.list('/notice');
+
+    // let data = {} as INotice;
+    // data.idx = "admin-20180306145700";
+    // data.writer = "admin";
+    // data.reg_date = "2018-03-06 14:57:00";
+    // data.update_date = "2018-03-06 14:57:00";
+    // data.title = "합격문 서비스 준비 중입니다. 3";
+    // data.content = "안녕하세요.<br><br>합격문에서 알려 드립니다.<br>합격문에서 알려 드립니다.";
+    // data.level = 0;
+    // data.order = 0;
+    // itemsRef.push(data);
+
+    this.notices = itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ ...c.payload.val(), expanded: false }));
+    });
+  }
+
+  expandItem(item){
+    item.expanded = !item.expanded;
+
+    if (this.selectedItem != item && this.selectedItem != null) {
+      this.selectedItem.expanded = false;
+    }
+    this.selectedItem = item;
+  }
 }
